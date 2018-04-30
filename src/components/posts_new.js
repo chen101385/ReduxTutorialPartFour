087fdab4350dd176020b74237997b9c64b8135d8
Christopher Chen <chen1385@gmail.com>
2018-04-29 19:19:12 -0700

    added validate function

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -26,8 +26,8 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -40,6 +40,26 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values) {
+    const errors = {};
+    //if errors is empty, form is fine to submit
+    //if errors has any properties, form is invalid. 
+    if (values.title.length < 3) {
+        errors.title = 'Title must be at least 3 characters';
+    } 
+    if (!values.title) {
+        errors.title = "Enter a title!";
+    }
+    if (!values.categories) {
+        errors.categories = 'Enter some categories!';
+    }
+    if (!values.content) {
+        errors.content = 'Enter some content please!'
+    }
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
