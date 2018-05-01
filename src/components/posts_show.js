5f0583f7470b2e19e7fe854cde744fb595297f5b
Christopher Chen <chen1385@gmail.com>
2018-04-30 17:30:35 -0700

    added checker for whether state updated within  posts_show component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -9,16 +9,26 @@ class PostsShow extends Component {
         this.props.fetchPost(id);
     }
     render() {
+        const { post } = this.props;
+
+        if (!post) {
+            return (
+                <div>Loading...</div>
+            )
+        }
+        
         return (
             <div>
-                Posts Show!
-                </div>
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
         );
     };
 }
-
+//ownProps === this.props for component(PostsShow)
 function mapStateToProps({ posts }, ownProps) {
-    return {posts: posts[ownProps.match.params.id]};
+    return { post: posts[ownProps.match.params.id] };
 }
 
 export default connect(mapStateToProps, { fetchPost })(PostsShow);
